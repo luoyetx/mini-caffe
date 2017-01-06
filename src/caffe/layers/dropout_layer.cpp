6b1ea666b71e1bd0f33a9a2f2a60e62225074dbd
zhangjie <luoyetx@gmail.com>
2017-01-06 11:02:32 +0800

    remove

--- src/caffe/layers/dropout_layer.cpp
+++ src/caffe/layers/dropout_layer.cpp
@@ -22,9 +22,6 @@ template <typename Dtype>
 void DropoutLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   NeuronLayer<Dtype>::Reshape(bottom, top);
-  // Set up the cache for random number generation
-  // ReshapeLike does not work because rand_vec_ is of Dtype uint
-  rand_vec_.Reshape(bottom[0]->shape());
 }
 
 template <typename Dtype>
@@ -32,17 +29,8 @@ void DropoutLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top) {
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
-  unsigned int* mask = rand_vec_.mutable_cpu_data();
   const int count = bottom[0]->count();
-  if (this->phase_ == TRAIN) {
-    // Create random numbers
-    caffe_rng_bernoulli(count, 1. - threshold_, mask);
-    for (int i = 0; i < count; ++i) {
-      top_data[i] = bottom_data[i] * mask[i] * scale_;
-    }
-  } else {
-    caffe_copy(bottom[0]->count(), bottom_data, top_data);
-  }
+  caffe_copy(bottom[0]->count(), bottom_data, top_data);
 }
 
 INSTANTIATE_CLASS(DropoutLayer);
