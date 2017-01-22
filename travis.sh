26fe931ea8778477dc14abb16070328fcfda1673
zhangjie <luoyetx@gmail.com>
2017-01-22 23:46:11 +0800

    fix

--- travis.sh
+++ travis.sh
@@ -9,7 +9,7 @@ make
 
 # before test
 URL_model="https://github.com/luoyetx/misc/blob/master/mini-caffe/model.zip?raw=true"
-if [ ! -f model.zip ] then
+if [ ! -f model.zip ]; then
   echo "Downloading model.zip from $URL_model"
   wget -O model.zip $URL_model
 fi
