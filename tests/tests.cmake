70592788b64acbcda288f442e3598b4e51dfb126
zhangjie <luoyetx@users.noreply.github.com>
2017-01-22 23:52:55 +0800

    add run net test (#7)
    * add run_net
    
    * update appveyor
    
    * fix appveyor
    
    * fix
    
    * run test
    
    * fix

---
+++ tests/tests.cmake
@@ -0,0 +1,2 @@
+add_executable(run_net ${CMAKE_CURRENT_LIST_DIR}/run_net.cpp)
+target_link_libraries(run_net caffe)
--- travis.sh
+++ travis.sh
@@ -2,6 +2,18 @@
 
 protoc -I="./src/proto" --cpp_out="./src/proto" "./src/proto/caffe.proto"
 
+# build
 mkdir build && cd build
 cmake ..
 make
+
+# before test
+URL_model="https://github.com/luoyetx/misc/blob/master/mini-caffe/model.zip?raw=true"
+if [ ! -f model.zip ]; then
+  echo "Downloading model.zip from $URL_model"
+  wget -O model.zip $URL_model
+fi
+unzip -o model.zip
+
+# test
+./run_net
