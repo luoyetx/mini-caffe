86ea711474f993afda9c864622a43cae5fac1320
zhangjie <luoyetx@gmail.com>
2017-02-22 21:02:44 +0800

    add wgan example

---
+++ example/wgan/main.cpp
@@ -0,0 +1,55 @@
+#include <cmath>
+#include <ctime>
+#include <random>
+#include <memory>
+#include <algorithm>
+#include <caffe/caffe.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <fstream>
+
+int main(int argc, char *argv) {
+  caffe::Net net("../models/wgan/g.prototxt");
+  net.CopyTrainedLayersFrom("../models/wgan/g.caffemodel");
+  // random noise
+  srand(time(NULL));
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::normal_distribution<float> nd(0, 1);
+  auto input = net.blob_by_name("data");
+  float *data = input->mutable_cpu_data();
+  const int n = input->count();
+  for (int i = 0; i < n; ++i) {
+    data[i] = nd(gen);
+  }
+  // forward
+  net.Forward();
+  // visualization
+  auto images = net.blob_by_name("gact5");
+  const int num = images->num();
+  const int channels = images->channels();
+  const int height = images->height();
+  const int width = images->width();
+  const int canvas_len = std::ceil(std::sqrt(num));
+  cv::Mat canvas(canvas_len*height, canvas_len*width, CV_8UC3);
+  auto clip = [](float x)->uchar {
+    const int val = static_cast<int>(x*127.5 + 127.5);
+    return std::max(0, std::min(255, val));
+  };
+  for (int i = 0; i < num; ++i) {
+    const int pos_y = (i / canvas_len)*height;
+    const int pos_x = (i % canvas_len)*width;
+    for (int y = 0; y < height; ++y) {
+      for (int x = 0; x < width; ++x) {
+        // BGR, mxnet model saves RGB
+        canvas.at<cv::Vec3b>(pos_y + y, pos_x + x)[0] = clip(images->data_at(i, 2, y, x));
+        canvas.at<cv::Vec3b>(pos_y + y, pos_x + x)[1] = clip(images->data_at(i, 1, y, x));
+        canvas.at<cv::Vec3b>(pos_y + y, pos_x + x)[2] = clip(images->data_at(i, 0, y, x));
+      }
+    }
+  }
+  std::vector<float> dd(images->cpu_data(), images->cpu_data()+images->count());
+  cv::imshow("gan-face", canvas);
+  cv::waitKey();
+  return 0;
+}
