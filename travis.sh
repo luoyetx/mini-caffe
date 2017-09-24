d9a4b703042e97691876c90308d154747dcf5782
zhangjie <luoyetx@gmail.com>
2017-09-24 13:46:59 +0800

    add test

--- travis.sh
+++ travis.sh
@@ -20,6 +20,7 @@ unzip -o model.zip
 # test
 ./run_net
 ./run_net_c
+./benchmark ./model/resnet.prototxt ./model/resnet.caffemodel 1 -1
 cd ..
 
 # java test
@@ -37,3 +38,7 @@ python3 --version
 python3 tests/test.py
 python3 setup.py build
 python3 setup.py clean
+cd ..
+
+# fuse bn
+python tools/fuse_bn.py --net ./build/model/resnet.prototxt --weight ./build/model/resnet.caffemodel
