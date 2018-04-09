ca5f38b5c19f40a49bfbc179485c23c0e86afdf5
nihuini <nihuini@tencent.com>
2018-04-09 14:17:04 +0800

    fix build script

--- mini-caffe.cmake
+++ mini-caffe.cmake
@@ -14,7 +14,7 @@ if(USE_JAVA)
 endif()
 
 # turn on C++11
-if(CMAKE_COMPILER_IS_GNUCXX)
+if(CMAKE_COMPILER_IS_GNUCXX OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang"))
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 endif()
 
@@ -32,7 +32,7 @@ elseif(ANDROID)
     include_directories(${CMAKE_CURRENT_LIST_DIR}/include
                         ${ANDROID_EXTRA_LIBRARY_PATH}/include)
     link_directories(${ANDROID_EXTRA_LIBRARY_PATH}/lib)
-    list(APPEND Caffe_LINKER_LIBS openblas protobuf)
+    list(APPEND Caffe_LINKER_LIBS openblas protobuf log)
   else(ANDROID_EXTRA_LIBRARY_PATH)
     message(FATAL_ERROR "ANDROID_EXTRA_LIBRARY_PATH must be set.")
   endif(ANDROID_EXTRA_LIBRARY_PATH)
